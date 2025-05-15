0cb97eab723eaae8685205e080994bbfa343d931
rgnter <maros.prejsa1@gmail.com>
2025-05-15 15:21:27 +0200

    some bugfixes, resource file and build system improvements

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -14,89 +14,89 @@
 
 namespace {
 
-  //! Settings.
-  struct Settings {
-    std::string _webInfoId;
-    alicia::WebInfo _webInfoContent;
-    std::string _executableProgram;
-    std::string _executableArguments;
-    bool _launch = true;
-  };
+//! Settings.
+struct Settings {
+  std::string _webInfoId;
+  alicia::WebInfo _webInfoContent;
+  std::string _executableProgram;
+  std::string _executableArguments;
+  bool _launch = true;
+};
 
-  //! Loads settings from file.
-  //! @param path Path to settings file.
-  //! @param settings Reference to settings.
-  void load_settings(const std::string_view& path, Settings& settings)
-  {
-    std::ifstream settingsFile(path.data());
-    if(!settingsFile.is_open()) {
-      throw std::runtime_error("The settings file does not exist.");
-    }
-
-    try
-    {
-      const auto json = nlohmann::json::parse(settingsFile);
-      settings._webInfoId = json["webInfoId"];
-
-      const nlohmann::json& webInfoContentJson = json["webInfoContent"];
-      settings._webInfoContent = {
-          .gameId = webInfoContentJson["GameId"],
-          .memberNo = webInfoContentJson["MemberNo"],
-          .loginId = webInfoContentJson["LoginId"],
-          .authKey = webInfoContentJson["AuthKey"],
-          .installUrl = webInfoContentJson["InstallUrl"],
-          .serverType = webInfoContentJson["ServerType"],
-          .serverInfo = webInfoContentJson["ServerInfo"],
-          .sex = static_cast<alicia::WebInfo::Sex>(webInfoContentJson["Sex"]),
-          .birthday = webInfoContentJson["Birthday"],
-          .wardNo = webInfoContentJson["WardNo"],
-          .cityCode = webInfoContentJson["CityCode"],
-          .zipCode = webInfoContentJson["ZipCode"],
-          .pcBangNo = webInfoContentJson["PcBangNo"],
-          .closeTime = webInfoContentJson["CloseTime"],
-      };
-
-      settings._executableProgram = json["executableProgram"];
-      settings._executableArguments = json["executableArguments"];
-      settings._launch = json["launch"];
-    }
-    catch(const nlohmann::json::exception& x)
-    {
-      throw x;
-    }
+//! Loads settings from file.
+//! @param path Path to settings file.
+//! @param settings Reference to settings.
+void load_settings(const std::string_view& path, Settings& settings)
+{
+  std::ifstream settingsFile(path.data());
+  if(!settingsFile.is_open()) {
+    throw std::runtime_error("The settings file does not exist.");
   }
 
-  //! Registers the alicia launch protocol in the registry.
-  //! @param name name of the protocol
-  //! @param path absolute path to the protocol command executable
-  void register_protocol(const std::string& name, const std::string& path)
+  try
   {
-    HKEY protocol, command;
-    int result;
+    const auto json = nlohmann::json::parse(settingsFile);
+    settings._webInfoId = json["webInfoId"];
 
-    result = RegCreateKeyA(HKEY_CLASSES_ROOT, (LPCSTR)name.c_str(), &protocol);
-    if(result != ERROR_SUCCESS)
-      throw std::runtime_error("failed to create registry key HKEY_CLASSES_ROOT\\a2launch");
+    const nlohmann::json& webInfoContentJson = json["webInfoContent"];
+    settings._webInfoContent = {
+        .gameId = webInfoContentJson["GameId"],
+        .memberNo = webInfoContentJson["MemberNo"],
+        .loginId = webInfoContentJson["LoginId"],
+        .authKey = webInfoContentJson["AuthKey"],
+        .installUrl = webInfoContentJson["InstallUrl"],
+        .serverType = webInfoContentJson["ServerType"],
+        .serverInfo = webInfoContentJson["ServerInfo"],
+        .sex = static_cast<alicia::WebInfo::Sex>(webInfoContentJson["Sex"]),
+        .birthday = webInfoContentJson["Birthday"],
+        .wardNo = webInfoContentJson["WardNo"],
+        .cityCode = webInfoContentJson["CityCode"],
+        .zipCode = webInfoContentJson["ZipCode"],
+        .pcBangNo = webInfoContentJson["PcBangNo"],
+        .closeTime = webInfoContentJson["CloseTime"],
+    };
 
-    result = RegSetValueEx(protocol, nullptr, 0, REG_SZ, (LPBYTE) "URL:a2launch Protocol", 22);
-    if(result != ERROR_SUCCESS)
-      throw std::runtime_error("failed to create registry value in 'HKEY_CLASSES_ROOT\\a2launch'");
-
-    result = RegSetValueEx(protocol, "URL Protocol", 0, REG_SZ, nullptr, 0);
-    if(result != ERROR_SUCCESS)
-      throw std::runtime_error("failed to create registry value in 'HKEY_CLASSES_ROOT\\a2launch'");
-
-    result = RegCreateKeyA(protocol, (LPCSTR) "shell\\open\\command", &command);
-    if(result != ERROR_SUCCESS)
-      throw std::runtime_error(
-          "failed to create registry key 'HKEY_CLASSES_ROOT\\shell\\open\\command'");
-
-    auto path_data = path.c_str();
-    result = RegSetValueEx(command, nullptr, 0, REG_SZ, (LPBYTE)path_data, strlen(path_data));
-    if(result != ERROR_SUCCESS)
-      throw std::runtime_error("failed to create registry value for command in "
-                               "'HKEY_CLASSES_ROOT\\a2launch\\shell\\open\\command'");
+    settings._executableProgram = json["executableProgram"];
+    settings._executableArguments = json["executableArguments"];
+    settings._launch = json["launch"];
   }
+  catch(const nlohmann::json::exception& x)
+  {
+    throw x;
+  }
+}
+
+//! Registers the alicia launch protocol in the registry.
+//! @param name name of the protocol
+//! @param path absolute path to the protocol command executable
+void register_protocol(const std::string& name, const std::string& path)
+{
+  HKEY protocol, command;
+  int result;
+
+  result = RegCreateKeyA(HKEY_CLASSES_ROOT, (LPCSTR)name.c_str(), &protocol);
+  if(result != ERROR_SUCCESS)
+    throw std::runtime_error("failed to create registry key HKEY_CLASSES_ROOT\\a2launch");
+
+  result = RegSetValueEx(protocol, nullptr, 0, REG_SZ, (LPBYTE) "URL:a2launch Protocol", 22);
+  if(result != ERROR_SUCCESS)
+    throw std::runtime_error("failed to create registry value in 'HKEY_CLASSES_ROOT\\a2launch'");
+
+  result = RegSetValueEx(protocol, "URL Protocol", 0, REG_SZ, nullptr, 0);
+  if(result != ERROR_SUCCESS)
+    throw std::runtime_error("failed to create registry value in 'HKEY_CLASSES_ROOT\\a2launch'");
+
+  result = RegCreateKeyA(protocol, (LPCSTR) "shell\\open\\command", &command);
+  if(result != ERROR_SUCCESS)
+    throw std::runtime_error(
+        "failed to create registry key 'HKEY_CLASSES_ROOT\\shell\\open\\command'");
+
+  auto path_data = path.c_str();
+  result = RegSetValueEx(command, nullptr, 0, REG_SZ, (LPBYTE)path_data, strlen(path_data));
+  if(result != ERROR_SUCCESS)
+    throw std::runtime_error("failed to create registry value for command in "
+                             "'HKEY_CLASSES_ROOT\\a2launch\\shell\\open\\command'");
+}
 
 } // namespace
 
@@ -128,11 +128,10 @@ int main(int argc, char** argv)
     return 0;
   }
 
-  // If launch is not set to true,
-  // do not spawn the game.
+  // If launch is not set to true, do not spawn the game.
   if(!settings._launch)
   {
-    spdlog::info("Not launching the game. Idling in the brackground until input from console.");
+    spdlog::info("Not launching the game. Idling in the background until input from console.");
     int a;
     std::cin >> a;
 
